bc273d02cef91f93cd088fddd427f1ba782f990d
Hammy Cooper <thehamstarr@gmail.com>
2023-03-22 17:16:47 +0800

    fix singleton

--- singleton.h
+++ singleton.h
@@ -4,17 +4,16 @@
 #include "ref_ptr.h"
 
 template< typename T >
-class Singleton : public Object< T >
+class Singleton : public object_t<>
 {
 public:
-    static  ref_ptr<T> instance() { return ref_ptr<T>( s_instance ); }
-
     template< typename... Args >
-    static inline auto get_or_create( Args&&... args )
+    static ref_ptr<T>& instance( Args&&... args )
     {
-        if( !s_instance ) s_instance = T::create( args... ).get();
-        return ref_ptr<T>( s_instance );
+        static ref_ptr<T> s_instance( new T( std::forward<Args>( args )... ) );
+        return s_instance;
     }
 protected:
-    static inline   T*  s_instance;
+    template< typename... Args >
+    Singleton( Args&&... args ) : object_t( std::forward<Args>( args )... ) {}
 };
