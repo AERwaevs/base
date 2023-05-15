7c6dcf61757ab93c2b1cfc440942158fe8022dda
Hammy Cooper <thehamstarr@gmail.com>
2023-05-15 17:31:22 +0800

    fix loguru cmake integration

--- inc/Base/log.h
+++ inc/Base/log.h
@@ -2,28 +2,25 @@
 
 //#include <loguru/loguru.hpp>
 //? this should be able to use angled brackets but fails?
-#include "../lib/loguru/loguru.hpp"
+#include "../../lib/loguru/loguru.hpp"
 
 namespace AEON::Log
 {
-    using namespace loguru;
-
-    inline void Init(   int&    argc,
-                        char**  argv )
+    inline void Init( int argc, char** argv )
     {
-        g_preamble_header       = false;
-        g_stderr_verbosity      = Verbosity_INFO;
-        g_internal_verbosity    = Verbosity_MAX;
-        g_flush_interval_ms     = 100;
-        init( argc, argv );
+        loguru::g_preamble_header       = false;
+        loguru::g_stderr_verbosity      = loguru::Verbosity_INFO;
+        loguru::g_internal_verbosity    = loguru::Verbosity_MAX;
+        loguru::g_flush_interval_ms     = 100;
+        loguru::init( argc, argv );
     }
 
     inline void New(    const std::string&  log_name    = "AEON", 
-                        FileMode            mode        = Truncate, 
-                        Verbosity           verbosity   = Verbosity_INFO )
+                        loguru::FileMode    mode        = loguru::Truncate, 
+                        loguru::Verbosity   verbosity   = loguru::Verbosity_INFO )
     {
         std::string file_name = log_name + ".log";
-        add_file( file_name.c_str(), mode, verbosity ); 
+        loguru::add_file( file_name.c_str(), mode, verbosity ); 
     }
 }
 
