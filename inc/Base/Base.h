84882d5ff776e4845e85a0b46c013998b7a6a35d
Hammy Cooper <thehamstarr@gmail.com>
2023-06-29 17:01:52 +0800

    remove base using statements

--- inc/Base/Base.h
+++ inc/Base/Base.h
@@ -26,23 +26,3 @@
 #include "interfaces/singleton.h"
 #include "io/read.h"
 #include "io/write.h"
-
-namespace aer
-{
-    using floats            = std::vector<float>;
-    using ints              = std::vector<int>;
-    using String            = std::string;
-    using SteadyClock       = std::chrono::steady_clock;
-    using TimePoint         = std::chrono::steady_clock::time_point;
-    using HighResClock      = std::chrono::high_resolution_clock;
-    using HighResTimePoint  = std::chrono::high_resolution_clock::time_point;
-
-    template< typename Type >
-    using Function      = std::function< Type >;
-    template< typename Type >
-    using Vector        = std::vector< Type >;
-    template< typename Type >
-    using List          = std::list< Type >;
-    template< typename Type1, typename Type2 >
-    using Map           = std::map< Type1, Type2 >;
-}
