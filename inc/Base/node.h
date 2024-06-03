8cd0c3c7fb4480271a56d63d8cb323ebe0874e1d
Hammy Cooper <thehamstarr@gmail.com>
2024-06-04 00:24:39 +0800

    Add default c/dtor to node

--- inc/Base/node.h
+++ inc/Base/node.h
@@ -8,10 +8,15 @@ namespace aer
 
 class Node : public Object
 {
+public:
+    Node() = default;
+    
     static void* operator new( size_t size )
     {
         return mem::alloc( size, mem::ALLOCATOR_AFFINITY_NODES );
     }
+protected:
+    ~Node() = default;
 };
 
 } // namespace aer
