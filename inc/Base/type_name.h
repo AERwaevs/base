84d40199c58a01502d5993a476778a7e5148aee5
ÆRwævs <thehamstarr@gmail.com>
2024-10-22 18:06:59 +0800

    Refactor type_name to add support for external type names

--- inc/Base/type_name.h
+++ inc/Base/type_name.h
@@ -4,11 +4,14 @@ namespace aer
 {
 
 template< typename T > constexpr auto type_name()            noexcept { return typeid(T).name(); }
-template< typename T > constexpr auto type_name( T& )        noexcept { return type_name<T>(); }
-template< typename T > constexpr auto type_name( const T& )  noexcept { return type_name<const T>(); }     
+template< typename T > constexpr auto type_name( const T& )  noexcept { return type_name<T>(); }
 
 }
 
 #define TYPE_NAME( T )\
-template<> auto type_name<T>()         noexcept { return #T; }\
-template<> auto type_name<const T>()   noexcept { return "const "#T; }
+template<> constexpr auto type_name<T>()       noexcept { return #T; }\
+template<> constexpr auto type_name<const T>() noexcept { return "const "#T; }
+
+#define EXT_TYPE_NAME( T )\
+template<> constexpr auto aer::type_name<T>()         noexcept { return #T; }\
+template<> constexpr auto aer::type_name<const T>()   noexcept { return "const "#T; }
