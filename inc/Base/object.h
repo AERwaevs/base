a5358e32ef13f8db0b0f39f6cc4e5f6a78b4c01b
AERwaevs <thehamstarr@gmail.com>
2024-11-23 21:04:30 +0800

    remove read and write methods

--- inc/Base/object.h
+++ inc/Base/object.h
@@ -1,13 +1,8 @@
 #pragma once
 
 #include <concepts>
-#include <filesystem>
-#include <ios>
-#include <fstream>
 
 #include "memory/Allocator.h"
-#include "memory/ref_counter.h"
-
 #include "memory/ref_ptr.h"
 
 #include "type_name.h"
@@ -37,40 +32,11 @@ public:
     template< typename Self, typename V > constexpr
     void accept( this Self&& self, V& visitor ) { visitor.visit( self ); }
 
-    template< typename Self > constexpr
-    bool write ( this Self&& self, std::string& path )
-    {
-        std::ofstream file( path, std::ios::binary );
-        AE_WARN_IF( !file.is_open(), "Failed to open file: %s", path.c_str() );
-        if( !file.is_open() ) return false;
-
-        file.write( reinterpret_cast<const char*>( self ), sizeof( Self ) );
-        file.close();
-
-        return true;
-    }
-
     inline auto ref_count( std::memory_order order = std::memory_order_relaxed ) const noexcept
     { 
         return _references.load(); 
     }
 protected:
-    template< typename T >
-    static inline auto read( const std::string& path )
-    {
-        std::ifstream file( path, std::ios::ate | std::ios::binary );
-        AE_FATAL_IF( !file.is_open(), "Failed to open file: %s", path.c_str() );
-
-        std::size_t         size( file.tellg() );
-        std::vector<char>   buffer( size );
-
-        file.seekg( 0 );
-        file.read( buffer.data(), size );
-        file.close();
-
-        return ref_ptr<T>( new T( buffer ) );
-    }
-    
              Object() noexcept = default;
     virtual ~Object() noexcept       = default;
              Object( Object&& )      = delete;
